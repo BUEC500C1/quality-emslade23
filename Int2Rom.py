dd4e104c40bae8a07159f781af2418d45e3f04ef
Elizabeth Slade <elizabethslade23@gmail.com>
2020-01-27 14:09:22 -0500

    development integer to roman numeral algorithm

--- Int2Rom.py
+++ Int2Rom.py
@@ -1,9 +1,50 @@
-"""
-Calculator library containing basic math operations.
-"""
 
-def add(first_term, second_term):
-    return first_term + second_term
+def arabicToRoman(input):
+    if int(input) > 4999:
+        return 'Too big a number for the Romans'
+    str_input = str(input)
+    return _arabicToRoman(str_input, len(str_input))
 
-def subtract(first_term, second_term):
-    return first_term - second_term
+def _arabicToRoman(input, length):
+    if length == 0:
+        return ''
+    if length == 1:
+        if input[0] == '9':
+            return 'IX'
+        elif input[0] >= '5':
+            return 'V' + ('I'*(int(input[0])-5))
+        elif input[0] == '4':
+            return 'I' + 'V'
+        elif input[0] > '0':
+            return 'I'*int(input[0])
+        else:
+            return _arabicToRoman(input[1:], length-1)
+    elif length == 4 and input[0] :
+
+        return 'M'*int(input[0]) + _arabicToRoman(input[1:], length-1)
+    elif length == 3:
+        if input[0] == '9':
+            return 'CM' + _arabicToRoman(input[1:], length-1)
+        elif input[0] >= '5':
+            return 'D' + ('C'*(int(input[0])-5)) + _arabicToRoman(input[1:], length-1)
+        elif input[0] == '4':
+            return 'C' + 'D' + _arabicToRoman(input[1:], length-1)
+        elif input[0] > '0':
+            return 'C'*int(input[0]) + _arabicToRoman(input[1:], length-1)
+        else:
+            return _arabicToRoman(input[1:], length-1)
+    elif length == 2:
+        if input[0] == '9':
+            return 'XC' + _arabicToRoman(input[1:], length-1)
+        elif input[0] >= '5':
+            return 'L' + ('X'*(int(input[0])-5)) + _arabicToRoman(input[1:], length-1)
+        elif input[0] == '4':
+            return 'X' + 'L' + _arabicToRoman(input[1:], length-1)
+        elif input[0] > '0':
+            return 'X'*int(input[0]) + _arabicToRoman(input[1:], length-1)
+        else:
+            return _arabicToRoman(input[1:], length-1)
+    else:
+        return 'Sorry, try again!'
+
+print(arabicToRoman(4999))
